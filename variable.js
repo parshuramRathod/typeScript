b33a5ca1acd77137fb5f9ef52864b849128603c7
Parshuram <parshuramrathod9540@gmail.com>
2022-11-09 00:10:44 +0530

    learned and installed typeScript
    learned and installed typeScript.

---
+++ variable.js
@@ -0,0 +1,9 @@
+var n1 = 10;
+console.log(n1);
+var s1 = "you are the creator of your destiny";
+var s2 = "powerfull people come from powerfull places";
+console.log(s1);
+console.log(s2);
+var b1 = true;
+var b2 = false;
+var b3;
---
+++ variable.ts
@@ -0,0 +1,10 @@
+var n1: number = 10;
+console.log(n1);
+var s1: string = "you are the creator of your destiny";
+var s2: string = `powerfull people come from powerfull places`;
+console.log(s1);
+console.log(s2);
+var b1: boolean = true;
+var b2 : boolean = false;
+var b3: boolean;
+
