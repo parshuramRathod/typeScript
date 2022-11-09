e556e7576ce71bbdb1e21bc20b80fc98449fa254
Parshuram <parshuramrathod9540@gmail.com>
2022-11-09 11:28:27 +0530

    learned popup, enum, stringtype,union basics
    learned popup, enum, stringtype,union basics

--- variable.js
+++ variable.js
@@ -7,3 +7,20 @@ console.log(s2);
 var b1 = true;
 var b2 = false;
 var b3;
+var a1 = {
+    productId: 1,
+    productName: "iPhone",
+    productPrice: 25000
+};
+console.log(a1);
+var a1 = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+};
+console.log(a1);
+// homodenous array 
+var arr1 = ['Angular', 'React', 'NodeJS', 'java'];
+// heterogenous array 
+var arr2 = ['Angular', 123456, true];
+console.log(arr2);
--- variable.ts
+++ variable.ts
@@ -5,6 +5,29 @@ var s2: string = `powerfull people come from powerfull places`;
 console.log(s1);
 console.log(s2);
 var b1: boolean = true;
-var b2 : boolean = false;
+var b2: boolean = false;
 var b3: boolean;
+
+var a1: any = {
+    productId: 1,
+    productName: "iPhone",
+    productPrice: 25000
+}
+console.log(a1);
+var a1: any = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+}
+
+console.log(a1);
+
+// homodenous array 
+var arr1 : Array<string> = ['Angular', 'React', 'NodeJS', 'java'];
+
+// heterogenous array 
+var arr2 : Array<any> = ['Angular', 123456, true];
+
+
+console.log(arr2);
 
